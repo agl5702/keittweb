a2576960170843433b144c7120d91e60448ec2cc
agl5702 <advasquez41@misena.edu.co>
2024-11-14 16:30:52 -0500

    change

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -7,7 +7,7 @@ import { Usuario } from './login.types';
   providedIn: 'root',
 })
 export class LoginService {
-  private apiUrl = 'https://guia-mango.onrender.com/'; // Cambia esto por la URL de tu API
+  private apiUrl = 'https://guia-mango.onrender.com'; // Cambia esto por la URL de tu API
 
   constructor(private http: HttpClient) {}
 
--- src/app/plagas/plagas.component.service.ts
+++ src/app/plagas/plagas.component.service.ts
@@ -7,7 +7,7 @@ import { LoginService } from '../login/login.service';
   providedIn: 'root',
 })
 export class PlagasComponentService {
-  private apiUrl = 'https://guia-mango.onrender.com/';
+  private apiUrl = 'https://guia-mango.onrender.com';
 
   constructor(private http: HttpClient, private loginService: LoginService) {}
 
